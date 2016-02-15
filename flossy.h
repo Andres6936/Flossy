926bd39914565205b817aeccb51baf60219eada0
rnlf <rnlf@users.noreply.github.com>
2016-02-15 23:05:42 +0100

    Continue work later

--- flossy.h
+++ flossy.h
@@ -2,9 +2,13 @@
 
 namespace flossy {
 
-template<typename OutputIterator, typename InputIterator, ...>
-void log(OutputIterator out, InputIterator start, InputIterator end, ...) {
-
+template<typename OutputIterator, typename InputIterator, typename... ElementTypes>
+void log(OutputIterator out, InputIterator start, InputIterator end, ElementTypes... elements) {
+  while(start != end) {
+    if(*start == '{') {
+      
+    }
+  }
 }
 
 }

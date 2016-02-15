35d9fccf6bf062cf418063372989badb5049ccf4
rnlf <rnlf@users.noreply.github.com>
2016-02-15 21:41:52 +0100

    Initial commit of almost empty file

---
+++ flossy.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace flossy {
+
+template<typename OutputIterator, typename InputIterator, ...>
+void log(OutputIterator out, InputIterator start, InputIterator end, ...) {
+
+}
+
+}

2de1fb0726f913cfffb7542d197811601fbbd2a9
rnlf <rnlf@users.noreply.github.com>
2016-02-16 22:53:09 +0100

    Basic format string parsing in place, needs more cleanup

--- example.cpp
+++ example.cpp
@@ -4,6 +4,11 @@
 #include <iterator>
 
 int main() {
-  std::string format = "{<+05.3f}";
+  std::string format = "xxxxx{>+05.3f}yyyyy";
   flossy::format(std::ostream_iterator<char>(std::cout), format.begin(), format.end(), 10.0);
+
+
+  std::string result = flossy::format("foobar{}narf", "hallo");
+
+  std::cout << result << std::endl;
 }
--- flossy.h
+++ flossy.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <regex>
+#include <iterator>
+#include <iostream>
+#include <exception>
+
 /*
   format: {(align)(sign)(0)(width)(.precision)(type)}
   align: [>_]
@@ -21,7 +26,9 @@ enum class conversion_format {
   hex,
   normal_float,
   scientific_float,
-  brace
+  brace,
+  normal,
+  fail
 };
 
 enum class conversion_alignment {
@@ -37,55 +44,44 @@ enum class conversion_positive_sign {
 };
 
 struct conversion_options {
+  conversion_format format;
   conversion_alignment alignment;
   conversion_positive_sign sign;
   bool zero_fill;
   int width;
   int precision;
-  conversion_format format;
+};
+
+template<typename CharT>
+const std::basic_regex<CharT> conversion_regex;
+
+class format_error: public std::invalid_argument {
+public:
+  using std::invalid_argument::invalid_argument;
 };
 
 template<typename InputIterator>
-conversion_alignment read_align(InputIterator *start, InputIterator end) {
-
-}
-
-template<typename InputIterator>
-conversion_positive_sign read_sign(InputIterator *start, InputIterator end) {
-
-}
-
-template<typename InputIterator>
-bool read_zero_fill(InputIterator *start, InputIterator end) {
-
-}
-
-template<typename InputIterator>
-int read_width(InputIterator *start, InputIterator end) {
-
-}
-
-template<typename InputIterator>
-int read_precision(InputIterator *start, InputIterator end) {
-
-}
-
-template<typename InputIterator>
-conversion_format read_format(InputIterator *start, InputIterator end) {
-
-}
-
-
-template<typename InputIterator>
 conversion_options read_conversion_options(InputIterator *start, InputIterator end) {
-  return {
-    read_align(start, end),
-    read_sign(start, end),
-    read_zero_fill(start, end),
-    read_width(start, end),
-    read_precision(start, end),
-    read_format(start, end)
-  };
+  typedef typename std::iterator_traits<InputIterator>::value_type char_type;
+
+  static const std::basic_regex<char_type> conversion_regex("^([>_<]?)([-+ ]?)(0?)([0-9]*)(\\.([0-9]+))?([dobxfe]?)(?=\\})");
+
+  std::match_results<InputIterator> match;
+  bool matched = std::regex_search(*start, end, match, conversion_regex);
+
+  if(!matched) {
+    throw format_error("Invalid format string");
+  }
+
+  *start = match.suffix().first;
+}
+
+template<typename OutputIterator, typename ValueType>
+void format_element(OutputIterator *out, conversion_options options, ValueType value) {
+  **out = '<';
+  ++(*out);
+  **out = '>';
+  ++(*out);
 }
 
 
@@ -94,18 +90,40 @@ void format(OutputIterator out, InputIterator start, InputIterator end) {
   std::copy(start, end, out);
 }
 
-template<typename OutputIterator, typename InputIterator, typename... ElementTypes>
-void format(OutputIterator out, InputIterator start, InputIterator end, ElementTypes... elements) {
+template<typename OutputIterator, typename InputIterator, typename FirstElement, typename... ElementTypes>
+void format(OutputIterator out, InputIterator start, InputIterator end, FirstElement first, ElementTypes... elements) {
   while(start != end) {
     auto const c = *start;
     if(c == '{') {
-      auto const options = read_conversion_options(&start, end);
+      ++start;
+      if(start != end) {
+        if(*start == '{') {
+          *out = '{';
+          ++out;
+        } else {
+          auto const options = read_conversion_options(&start, end);
+          ++start;
+          format_element(&out, options, first);
+          format(out, start, end, elements...);
+          return;
+        }
+      } else {
+        throw format_error("Invalid format string");
+      }
     } else {
       *out = c;
+      ++(*out);
     }
+
     ++start;
-    ++out;
   }
+}
+
+template<typename... ElementTypes>
+std::string format(std::string format_str, ElementTypes... elements) {
+  std::string result;
+  format(std::back_inserter(result), format_str.begin(), format_str.end(), elements...);
+  return result;
 }
 
 }

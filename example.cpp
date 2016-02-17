8e92c4983c6e3437c0c7cbfb1b2b7165f790c8b0
rnlf <rnlf@users.noreply.github.com>
2016-02-17 15:22:54 +0100

    switched back to hand written format option parser, I cannot accept a 25k asm instruction regex parser

--- example.cpp
+++ example.cpp
@@ -4,11 +4,8 @@
 #include <iterator>
 
 int main() {
-  std::string format = "xxxxx{>+05.3f}yyyyy";
-  flossy::format(std::ostream_iterator<char>(std::cout), format.begin(), format.end(), 10.0);
 
-
-  std::string result = flossy::format("foobar{}narf", "hallo");
+  std::string result = flossy::format("foobar{>+09.3f}narf", "hallo");
 
   std::cout << result << std::endl;
 }
--- flossy.h
+++ flossy.h
@@ -44,12 +44,12 @@ enum class conversion_positive_sign {
 };
 
 struct conversion_options {
-  conversion_format format;
-  conversion_alignment alignment;
-  conversion_positive_sign sign;
-  bool zero_fill;
-  int width;
-  int precision;
+  conversion_alignment alignment = conversion_alignment::left;
+  conversion_positive_sign sign = conversion_positive_sign::none;
+  bool zero_fill = false;
+  int width = 0;
+  int precision = 0;
+  conversion_format format = conversion_format::normal;
 };
 
 template<typename CharT>
@@ -60,6 +60,7 @@ public:
   using std::invalid_argument::invalid_argument;
 };
 
+/*
 template<typename InputIterator>
 conversion_options read_conversion_options(InputIterator *start, InputIterator end) {
   typedef typename std::iterator_traits<InputIterator>::value_type char_type;
@@ -74,6 +75,132 @@ conversion_options read_conversion_options(InputIterator *start, InputIterator e
   }
 
   *start = match.suffix().first;
+}
+*/
+
+
+
+template<typename InputIterator>
+conversion_options read_conversion_options(InputIterator &it, InputIterator end) {
+  conversion_options options;
+
+  enum class step_name {
+    align, sign, fill, width, precision_dot, precision_read, format, close
+  };
+
+  step_name step = step_name::align;
+  while(it != end) {
+    auto const c = *it;
+    switch(step) {
+    case step_name::align:
+      switch(c) {
+      case '_':
+        options.alignment = conversion_alignment::center;
+        ++it;
+        break;
+      case '>':
+        options.alignment = conversion_alignment::right;
+        ++it;
+        break;
+      case 'y':
+        ++it;
+      }
+      step = step_name::sign;
+      break;
+
+    case step_name::sign:
+      switch(c) {
+      case ' ':
+        options.sign = conversion_positive_sign::space;
+        ++it;
+        break;
+      case '+':
+        options.sign = conversion_positive_sign::plus;
+        ++it;
+        break;
+      case '-':
+        ++it;
+      }
+      step = step_name::fill;
+      break;
+
+    case step_name::fill:
+      if(c == '0') {
+        ++it;
+        options.zero_fill = true;
+      }
+      step = step_name::width;
+      break;
+
+    case step_name::width:
+      if(c >= '0' && c <= '9') {
+        options.width = options.width * 10 + (c - '0');
+        ++it;
+      } else {
+        step = step_name::precision_dot;
+      }
+      break;
+
+    case step_name::precision_dot:
+      if(c == '.') {
+        step = step_name::precision_read;
+        ++it;
+      } else {
+        // skip precision, recheck current char
+        step = step_name::format;
+      }
+      break;
+
+    case step_name::precision_read:
+      if(c >= '0' && c <= '9') {
+        options.precision = options.precision * 10 + (c - '0');
+        ++it;
+      } else {
+        step = step_name::format;
+      }
+      break;
+
+    case step_name::format:
+      switch(c) {
+      case 'd':
+        options.format = conversion_format::decimal;
+        ++it;
+        break;
+      case 'o':
+        options.format = conversion_format::octal;
+        ++it;
+        break;
+      case 'b':
+        options.format = conversion_format::binary;
+        ++it;
+        break;
+      case 'x':
+        options.format = conversion_format::hex;
+        ++it;
+        break;
+      case 'f':
+        options.format = conversion_format::normal_float;
+        ++it;
+        break;
+      case 'e':
+        options.format = conversion_format::scientific_float;
+        ++it;
+        break;
+      default:
+        step = step_name::close;
+      }
+      break;
+
+    case step_name::close:
+      if(c == '}') {
+        ++it;
+        return options;
+      } else {
+        throw format_error(std::string("Invalid character in format string: '") + c + "'");
+      }
+      break;
+    }
+  }
 }
 
 template<typename OutputIterator, typename ValueType>
@@ -101,14 +228,19 @@ void format(OutputIterator out, InputIterator start, InputIterator end, FirstEle
           *out = '{';
           ++out;
         } else {
-          auto const options = read_conversion_options(&start, end);
+          auto format_end = std::find(start, end, '}');
+          if(format_end == end) {
+            //throw format_error("Missing } in format string");
+          }
+          auto const options = read_conversion_options(start, format_end);
+          start = format_end;
           ++start;
           format_element(&out, options, first);
           format(out, start, end, elements...);
           return;
         }
       } else {
-        throw format_error("Invalid format string");
+        //throw format_error("Invalid format string");
       }
     } else {
       *out = c;

41a82024f90914dd6d54013e4ddcba2ad271e491
rnlf <rnlf@users.noreply.github.com>
2016-02-23 11:41:16 +0100

    double_conversion streamlined enough for integration into flossy

--- dc/main.cpp
+++ dc/main.cpp
@@ -7,6 +7,7 @@ int main() {
   int len, dec;
   fast_dtoa(123.123443234875638475638745638745638746583745687, fast_dtoa_mode::shortest, 0, buffer<char>(buf, sizeof(buf)), &len, &dec);
   printf("%se%d\n", buf, dec-1);
+  printf("%d\n", +0.0 == -0.0);
 
   foo();
 }
